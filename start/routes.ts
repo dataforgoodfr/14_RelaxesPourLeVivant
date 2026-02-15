cabbacf02113b4ef6664d54c9e1accf7e3136997
Yoann Bourdex <yoann.bourdex@gmail.com>
2026-02-15 19:59:29 +0100

    chore(routes): apply naming convention for AudiencesController

--- start/routes.ts
+++ start/routes.ts
@@ -12,11 +12,11 @@ import { middleware } from './kernel.js'
 import env from '#start/env'
 const HomeController = () => import('#controllers/home_controller')
 const AuthController = () => import('#controllers/auth_controller')
-const audiencesController = () => import('#controllers/audiences_controller')
+const AudiencesController = () => import('#controllers/audiences_controller')
 
 router.on('/welcome').render('pages/welcome').use(middleware.guest())
 router.get('/audiences', [HomeController, 'home']).use(middleware.auth())
-router.get('/audiences/:id', [audiencesController, 'get']).use(middleware.auth())
+router.get('/audiences/:id', [AudiencesController, 'get']).use(middleware.auth())
 router.on('/sign-up').render('pages/auth/sign_up')
 router.on('/sign-in').render('pages/auth/sign_in').use(middleware.guest())
 
