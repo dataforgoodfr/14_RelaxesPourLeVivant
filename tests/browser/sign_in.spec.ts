6e53dd8c07c1d39f97ad34a0ff312895ba16d2d7
Yoann Bourdex <yoann.bourdex@gmail.com>
2026-03-04 11:19:36 +0100

    chore(test): add tests on signin

---
+++ tests/browser/sign_in.spec.ts
@@ -0,0 +1,49 @@
+import { UserFactory } from '#database/factories/user_factory'
+import Utilisateur from '#models/user'
+import { test } from '@japa/runner'
+
+test
+  .group('signin', () => {
+    test('user is unknown', async ({ visit }) => {
+      const page = await visit('/sign-in')
+
+      await page.getByLabel('Email').fill('unkown-user@mail.com')
+      await page.getByLabel('Mot de passe').fill('random-password')
+      await page.getByRole('button').click()
+
+      await page.assertVisible(page.getByText('Mot de passe ou email incorrect'))
+    })
+
+    test('user is known and approved', async ({ visit }) => {
+      const user = await UserFactory.merge({ password: 'random-password' })
+        .apply('approved')
+        .create()
+
+      const page = await visit('/sign-in')
+
+      await page.getByLabel('Email').fill(user.email)
+      await page.getByLabel('Mot de passe').fill('random-password')
+      await page.getByRole('button').click()
+
+      await page.assertPath('/audiences')
+    })
+
+    test('user is known but not approved', async ({ visit }) => {
+      const user = await UserFactory.merge({ password: 'random-password' }).create()
+
+      const page = await visit('/sign-in')
+
+      await page.getByLabel('Email').fill(user.email)
+      await page.getByLabel('Mot de passe').fill('random-password')
+      await page.getByRole('button').click()
+
+      await page.assertVisible(
+        page.getByText(
+          "Vous n'êtes pas autorisé, si vous pensez que c'est un problème contactez nous"
+        )
+      )
+    })
+  })
+  .teardown(async () => {
+    await Utilisateur.truncate(true)
+  })
