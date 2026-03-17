e362647dfe5f74bbdaf18c16db912c6207aa01af
Yoann Bourdex <yoann.bourdex@gmail.com>
2026-03-17 10:59:37 +0100

    fix(import): refresh the auto-increment after an import

--- app/controllers/imports_controller.ts
+++ app/controllers/imports_controller.ts
@@ -1,6 +1,7 @@
 import { importCsvValidator } from '#validators/import_csv'
 import type { HttpContext } from '@adonisjs/core/http'
 import db from '@adonisjs/lucid/services/db'
+import type { TransactionClientContract } from '@adonisjs/lucid/types/database'
 import { readFile, rm } from 'node:fs/promises'
 
 export default class ImportsController {
@@ -9,10 +10,16 @@ export default class ImportsController {
 
     const data = await readFile(csv.tmpPath!, { encoding: 'base64' })
 
-    await db.knexRawQuery(
+    const trx = await db.transaction()
+
+    await trx.knexRawQuery(
       `COPY ?? FROM PROGRAM 'echo ${data} | base64 -d' WITH (FORMAT csv, HEADER, DELIMITER ';', FORCE_NULL *)`,
       [request.param('table')]
     )
+
+    await this.refreshAutoIncrement(request.param('table'), trx)
+
+    await trx.commit()
 
     try {
       await rm(csv.tmpPath!)
@@ -21,5 +28,30 @@ export default class ImportsController {
     }
 
     return response.ok('upload success')
+  }
+
+  private async refreshAutoIncrement(table: string, trx: TransactionClientContract) {
+    const resultSeqNameQuery = await trx.knexRawQuery(
+      `SELECT pg_get_serial_sequence as seqname FROM pg_get_serial_sequence(?, 'id')`,
+      [table]
+    )
+
+    if (!resultSeqNameQuery.rowCount) {
+      return
+    }
+
+    const resultMaxIdQuery = await trx.knexRawQuery(
+      'SELECT COALESCE(MAX(id), 0) as maxid FROM ??',
+      [table]
+    )
+
+    if (!resultMaxIdQuery.rowCount) {
+      return
+    }
+
+    const seqName = resultSeqNameQuery.rows.at(0).seqname
+    const maxId = resultMaxIdQuery.rows.at(0).maxid
+
+    await trx.knexRawQuery(`ALTER SEQUENCE ${seqName} RESTART WITH ${maxId + 1}`)
   }
 }
