4bbf6a7a75898d6085ae7ab5dd48ec23068b5382
Yoann Bourdex <yoann.bourdex@gmail.com>
2026-03-21 09:09:36 +0100

    refactor(audiences): move timeline algo to mappers

--- tests/unit/timeline_mapper.spec.ts
+++ tests/unit/timeline_mapper.spec.ts
@@ -1,19 +1,19 @@
+import { TimelineDataMapper } from '#controllers/mappers/timeline_mapper'
 import { test } from '@japa/runner'
-import { AudienceService } from '#services/audience_service'
 
-test.group('Filter timeline algorithm', () => {
-  const service = new AudienceService()
+test.group('Timeline mapper algorithm', () => {
+  const timelineDataMapper = new TimelineDataMapper()
 
-  const mockEvent = (id: number, date: string | null, testProperty?: string) => ({
+  const mockEvent = (id: number, date?: string, testProperty?: string) => ({
     id,
-    date_de_decision: date ?? undefined,
-    degre_de_juridiction: testProperty ?? null,
+    date_de_decision: date,
+    degre_de_juridiction: testProperty,
   })
 
   test('should return unaltered event objects', async ({ assert }) => {
     const text = 'This text should remain unaltered'
     const timeline = [mockEvent(1, '2020-01-01', text)]
-    const result = (service as any).filterTimeline(timeline, 1)
+    const result = timelineDataMapper.map({ timeline, id: 1 })
     assert.equal(result[0].id, 1)
     assert.equal(result[0].degre_de_juridiction, text)
     assert.equal(result[0].date_de_decision, '2020-01-01')
@@ -21,7 +21,7 @@ test.group('Filter timeline algorithm', () => {
 
   test('should return all events when they are less than 4', ({ assert }) => {
     const timeline = [mockEvent(1, '2024-01-10'), mockEvent(2, '2024-01-20')]
-    const result = (service as any).filterTimeline(timeline, 1)
+    const result = timelineDataMapper.map({ timeline, id: 1 })
     assert.lengthOf(result, 2)
     assert.equal(result[0].id, 1)
   })
@@ -35,7 +35,7 @@ test.group('Filter timeline algorithm', () => {
       mockEvent(5, '2024-05-01'),
     ]
 
-    const result = (service as any).filterTimeline(timeline, 3)
+    const result = timelineDataMapper.map({ timeline, id: 3 })
 
     assert.lengthOf(result, 4, 'Timeline returned should contain 4 events')
     assert.equal(result[1].id, 3, 'Current audience should be the second item (index 1)')
@@ -55,14 +55,14 @@ test.group('Filter timeline algorithm', () => {
       mockEvent(4, '2024-04-01'),
       mockEvent(5, '2024-05-01'),
     ]
-    const result = (service as any).filterTimeline(timeline, 1)
+    const result = timelineDataMapper.map({ timeline, id: 1 })
     assert.lengthOf(result, 4)
     assert.equal(result[0].id, 1, 'Current audience is the first because no past event')
   })
 
   test('should handle correctly empty dates', ({ assert }) => {
-    const timeline = [mockEvent(1, '2024-01-01'), mockEvent(2, null), mockEvent(3, '2023-01-01')]
-    const result = (service as any).filterTimeline(timeline, 1)
+    const timeline = [mockEvent(1, '2024-01-01'), mockEvent(2), mockEvent(3, '2023-01-01')]
+    const result = timelineDataMapper.map({ timeline, id: 1 })
     assert.equal(result[0].id, 3, '2023 should be first')
     assert.equal(result[2].id, 2, 'Empry date should be the last (year 2199)')
   })
@@ -75,13 +75,13 @@ test.group('Filter timeline algorithm', () => {
       mockEvent(4, '2024-04-01'),
       mockEvent(5, '2024-05-01'),
     ]
-    const result = (service as any).filterTimeline(timeline, 5)
+    const result = timelineDataMapper.map({ timeline, id: 5 })
     assert.lengthOf(result, 4, 'Should fill with past events if no future events left')
   })
 
   test('should return 1 event there is only one event', ({ assert }) => {
     const timeline = [mockEvent(1, '2024-01-01')]
-    const result = (service as any).filterTimeline(timeline, 1)
+    const result = timelineDataMapper.map({ timeline, id: 1 })
     assert.lengthOf(result, 1)
   })
 })
