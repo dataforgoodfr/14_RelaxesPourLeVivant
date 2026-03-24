84303cc99e42673ec56baf220ddbbd17d48feba9
Sebastien Seguin <80358666+sebaseg-dev@users.noreply.github.com>
2026-03-24 07:08:07 +0100

    feat: using npm package for phosphor icons (instead of cdn) fix: absence of information was tested with '??' but property does exists, prefering testing value (@if @else @endif)

--- start/view.ts
+++ start/view.ts
@@ -1,6 +1,11 @@
 import string from '@adonisjs/core/helpers/string'
 import edge from 'edge.js'
 import { DateTime } from 'luxon'
+import { edgeIconify, addCollection } from 'edge-iconify'
+import { icons as phIcons } from '@iconify-json/ph'
 
 edge.global('DateTime', DateTime)
 edge.global('stringHelper', string)
+
+addCollection(phIcons)
+edge.use(edgeIconify)
