5891e7dd7ed2ba8ed74de59a4a9b63ac7f5c2f6c
Yoann Bourdex <yoann.bourdex@gmail.com>
2026-04-03 11:44:05 +0200

    feat: new search component (#151)
    * feat: add dateDesFaits, dateDeLaDecision and dateAudience to search logic
    
    * feat: add change event to datepicker and multiselect
    
    * feat: add data-* props to datepicker and multiselect
    
    * feat: add morefilters component
    
    * reafactor: use js instead of template to init values in morefilters component
    
    * feat: strengthen the date format in searchQueryValidator

--- app/validators/search_query.ts
+++ app/validators/search_query.ts
@@ -1,10 +1,13 @@
 import vine from '@vinejs/vine'
 
+const dateFormat = /\d{4}-\d{2}-\d{2}/
+
 export const searchQueryValidator = vine.compile(
   vine.object({
     search: vine.string().optional(),
-    startDate: vine.string().optional(),
-    endDate: vine.string().optional(),
+    dateDesFaits: vine.array(vine.string().regex(dateFormat)).fixedLength(2).optional(),
+    dateDeLaDecision: vine.array(vine.string().regex(dateFormat)).fixedLength(2).optional(),
+    dateAudience: vine.array(vine.string().regex(dateFormat)).fixedLength(2).optional(),
     decision: vine.string().optional(),
     juridiction: vine.string().optional(),
     chefDePrevention: vine.array(vine.string()).optional(),
