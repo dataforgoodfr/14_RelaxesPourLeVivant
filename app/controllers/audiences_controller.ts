c38d6499a3b2e22bd9b4dccf9d72cc1479bd853e
jbuiquan <jbuiquan@users.noreply.github.com>
2026-04-09 08:24:38 +0200

    feat: demande acces recit audience #147

--- app/controllers/audiences_controller.ts
+++ app/controllers/audiences_controller.ts
@@ -40,28 +40,6 @@ export default class AudiencesController {
   }
 
   /**
-   * Returns the blob file associated to the recit of a published audience.
-   * @param HttpContext
-   * @returns
-   */
-  async getRecitFile({ request, response }: HttpContext) {
-    const audience = await this.audienceService.getAudiencePubliee(request.param('id'))
-    const recit = audience.recit_d_audience?.find((r) => r.id === request.param('recitId'))
-
-    if (!recit) {
-      return response.notFound('Récit d’audience non trouvé')
-    }
-
-    const result = await this.nocodbService.fetchAttachmentFile(recit.path)
-    if (result.body) {
-      response.header('Content-Type', 'application/octet-stream')
-      response.header('Content-Disposition', `inline; filename="${recit.title}"`)
-      return response.stream(Readable.fromWeb(result.body))
-    }
-    return response.noContent()
-  }
-
-  /**
    * Returns the blob file associated to the jugement of a published audience.
    * @param HttpContext
    * @returns
