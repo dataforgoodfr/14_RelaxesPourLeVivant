1a74b48b56cbe813888dc6920511c1ff413a5e09
Jean Bui Quang <jean.buiquang@etik.com>
2026-04-16 20:09:46 +0000

    fix: date des faits (complete version with skipped count)

--- app/controllers/mappers/timeline_mapper.ts
+++ app/controllers/mappers/timeline_mapper.ts
@@ -1,9 +1,48 @@
-type TimelineItem = {
+/**
+ * Resulting type of the `searchAudiences` query.
+ */
+export type TimelineDbResultItem = {
   id: number
   date_de_decision?: string
   degre_de_juridiction?: string
   decision_pour_les_infractions_principales?: string
   type_de_peine_pour_les_infractions_principales?: string
+}
+
+/*
+ * Timeline item to be rendered in the view, with or without date.
+ * Three types :
+ * - date des faits (type: 'faits', date: string)
+ * - audience with date (type: 'audience', date: string)
+ * - audience à venir (type: 'audience_a_venir', date: undefined)
+ */
+export type TimelineItem =
+  | TimelineDateDesFaitsItem
+  | TimelineAudienceItem
+  | TimelineAudienceAVenirItem
+
+type BaseAudienceItem = {
+  id: number
+  degre_de_juridiction?: string
+  decision_pour_les_infractions_principales?: string
+  type_de_peine_pour_les_infractions_principales?: string
+  skipped_before?: number
+  skipped_after?: number
+}
+
+export type TimelineAudienceItem = BaseAudienceItem & {
+  type: 'audience'
+  date: string
+}
+
+export type TimelineAudienceAVenirItem = BaseAudienceItem & {
+  type: 'audience_a_venir'
+  date: null
+}
+
+export type TimelineDateDesFaitsItem = {
+  type: 'faits'
+  date: string
 }
 
 /**
@@ -12,40 +51,76 @@ type TimelineItem = {
  */
 export class TimelineDataMapper {
   constructor(
-    private readonly maximumNumberOfEvents = 3,
+    private readonly maximumNumberOfEvents = 4, // date des faits will be first
     private readonly preferredPositionForCurrentAudience = 1
   ) {}
 
-  map(audience: { timeline: TimelineItem[]; id: number }) {
-    const eventsWithDate: (TimelineItem & { date_de_decision: string })[] = []
-    const eventsWithoutDate: TimelineItem[] = []
+  map(audience: {
+    id: number
+    date_des_faits: string | null
+    timeline: TimelineDbResultItem[]
+  }): TimelineItem[] {
+    const eventsWithDate: TimelineAudienceItem[] = []
+    const eventsWithoutDate: TimelineAudienceAVenirItem[] = []
+
+    // We need to reserve one slot for date des faits if it exists
+    const maximumNumberOfEvents = this.maximumNumberOfEvents - (audience.date_des_faits ? 1 : 0)
+
+    /**
+     * Build the final timeline with the date des faits at first position if it exists, then the sorted audiences.
+     * @param sortedAudiences
+     * @returns
+     */
+    function buildTimeline(
+      sortedAudiences: Array<TimelineAudienceItem | TimelineAudienceAVenirItem>
+    ): TimelineItem[] {
+      const dateDesFaitsItem: TimelineDateDesFaitsItem | null = audience.date_des_faits
+        ? {
+            type: 'faits',
+            date: audience.date_des_faits,
+          }
+        : null
+      if (dateDesFaitsItem) {
+        return [dateDesFaitsItem, ...sortedAudiences]
+      }
+      return [...sortedAudiences]
+    }
 
     for (const item of audience.timeline) {
       if (item.date_de_decision) {
-        eventsWithDate.push(item as TimelineItem & { date_de_decision: string })
+        eventsWithDate.push({
+          ...item,
+          type: 'audience',
+          date: item.date_de_decision,
+        })
       } else {
-        eventsWithoutDate.push(item)
+        eventsWithoutDate.push({
+          ...item,
+          type: 'audience_a_venir',
+          date: null,
+        })
       }
     }
 
-    eventsWithDate.sort(
-      (a, b) => new Date(a.date_de_decision).getTime() - new Date(b.date_de_decision).getTime()
-    )
+    eventsWithDate.sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime())
 
     eventsWithoutDate.sort((a, b) => Number(a.id) - Number(b.id))
 
     const sortedTimeline = [...eventsWithDate, ...eventsWithoutDate]
 
     // When total events < maximum number of events, early return of sorted dates
-    if (sortedTimeline.length <= this.maximumNumberOfEvents) {
-      return sortedTimeline
+    if (sortedTimeline.length <= maximumNumberOfEvents) {
+      return buildTimeline(sortedTimeline)
     }
 
     // Splitting sortedTimeline into 3 groups (pasts, current, futures)
     const currentIndex = sortedTimeline.findIndex((t) => Number(t.id) === audience.id)
 
+    // Maximum number of events > complete timeline, audience is not present in the timeline, we return the most recent events trimmed to the maximum number of events
     if (currentIndex === -1) {
-      return sortedTimeline.slice(0, this.maximumNumberOfEvents)
+      const trimmedSize = sortedTimeline.length - maximumNumberOfEvents
+      sortedTimeline[maximumNumberOfEvents - 1].skipped_after = trimmedSize
+      return buildTimeline(sortedTimeline.slice(0, maximumNumberOfEvents))
     }
 
     const currentEvent = sortedTimeline[currentIndex]
@@ -56,19 +131,30 @@ export class TimelineDataMapper {
     let numberOfPastEvents = Math.min(this.preferredPositionForCurrentAudience, pastEvents.length)
     const numberOfFutureEvents = Math.min(
       futureEvents.length,
-      this.maximumNumberOfEvents - (1 + numberOfPastEvents)
+      maximumNumberOfEvents - (1 + numberOfPastEvents)
     )
-    if (numberOfPastEvents + numberOfFutureEvents !== this.maximumNumberOfEvents - 1) {
+    if (numberOfPastEvents + numberOfFutureEvents !== maximumNumberOfEvents - 1) {
       numberOfPastEvents = Math.min(
         pastEvents.length,
-        this.maximumNumberOfEvents - 1 - numberOfFutureEvents
+        maximumNumberOfEvents - 1 - numberOfFutureEvents
       )
     }
 
-    return [
-      ...pastEvents.slice(pastEvents.length - numberOfPastEvents),
+    const pastEventsStartIndex = pastEvents.length - numberOfPastEvents
+    const trimmedBeforePastEvents = pastEventsStartIndex
+    if (trimmedBeforePastEvents > 0) {
+      pastEvents[pastEventsStartIndex].skipped_before = trimmedBeforePastEvents
+    }
+    const futureEventsEndIndex = numberOfFutureEvents
+    const trimmedAfterFutureEvents = futureEvents.length - numberOfFutureEvents
+    if (trimmedAfterFutureEvents > 0) {
+      futureEvents[futureEventsEndIndex - 1].skipped_after = trimmedAfterFutureEvents
+    }
+
+    return buildTimeline([
+      ...pastEvents.slice(pastEventsStartIndex),
       currentEvent,
-      ...futureEvents.slice(0, numberOfFutureEvents),
-    ]
+      ...futureEvents.slice(0, futureEventsEndIndex),
+    ])
   }
 }
--- tests/unit/timeline_mapper.spec.ts
+++ tests/unit/timeline_mapper.spec.ts
@@ -1,4 +1,8 @@
-import { TimelineDataMapper } from '#controllers/mappers/timeline_mapper'
+import {
+  TimelineAudienceAVenirItem,
+  TimelineAudienceItem,
+  TimelineDataMapper,
+} from '#controllers/mappers/timeline_mapper'
 import { test } from '@japa/runner'
 
 test.group('Timeline mapper algorithm', () => {
@@ -10,20 +14,21 @@ test.group('Timeline mapper algorithm', () => {
     degre_de_juridiction: testProperty,
   })
 
-  test('should return unaltered event objects', async ({ assert }) => {
+  test('should return unaltered event data', async ({ assert }) => {
     const text = 'This text should remain unaltered'
     const timeline = [mockEvent(1, '2020-01-01', text)]
-    const result = timelineDataMapper.map({ timeline, id: 1 })
-    assert.equal(result[0].id, 1)
-    assert.equal(result[0].degre_de_juridiction, text)
-    assert.equal(result[0].date_de_decision, '2020-01-01')
+    const result = timelineDataMapper.map({ timeline, id: 1, date_des_faits: null })
+    assert.equal(result[0].type, 'audience')
+    assert.equal((result[0] as TimelineAudienceItem).id, 1)
+    assert.equal((result[0] as TimelineAudienceItem).degre_de_juridiction, text)
+    assert.equal((result[0] as TimelineAudienceItem).date, '2020-01-01')
   })
 
-  test('should return all events when they are less than 4', ({ assert }) => {
+  test('should return all events when they are less than maximumNumberOfEvents', ({ assert }) => {
     const timeline = [mockEvent(1, '2024-01-10'), mockEvent(2, '2024-01-20')]
-    const result = timelineDataMapper.map({ timeline, id: 1 })
+    const result = timelineDataMapper.map({ timeline, id: 1, date_des_faits: null })
     assert.lengthOf(result, 2)
-    assert.equal(result[0].id, 1)
+    assert.equal((result[0] as TimelineAudienceItem).id, 1)
   })
 
   test('should move the current audience at prefered index (#1)', ({ assert }) => {
@@ -35,16 +40,33 @@ test.group('Timeline mapper algorithm', () => {
       mockEvent(5, '2024-05-01'),
     ]
 
-    const result = timelineDataMapper.map({ timeline, id: 3 })
+    const result = timelineDataMapper.map({ timeline, id: 3, date_des_faits: null })
 
     assert.lengthOf(result, 4, 'Timeline returned should contain 4 events')
-    assert.equal(result[1].id, 3, 'Current audience should be the second item (index 1)')
+    assert.equal(result[1].type, 'audience', 'Current audience should be a TimelineAudienceItem')
     assert.equal(
-      result[0].id,
+      (result[1] as TimelineAudienceItem).id,
+      3,
+      'Current audience should be the second item (index 1)'
+    )
+    assert.equal(result[0].type, 'audience', 'Past audience should be a TimelineAudienceItem')
+    assert.equal(
+      (result[0] as TimelineAudienceItem).id,
       2,
       'Timeline should have 1 past event in the first position(index 0)'
     )
-    assert.equal(result[3].id, 5, 'Timeline should contain 2 future events')
+    assert.equal(result[2].type, 'audience', 'Future audience should be a TimelineAudienceItem')
+    assert.equal(
+      (result[2] as TimelineAudienceItem).id,
+      4,
+      'Timeline should contain 1 future event in the third position (index 2)'
+    )
+    assert.equal(result[3].type, 'audience', 'Future audience should be a TimelineAudienceItem')
+    assert.equal(
+      (result[3] as TimelineAudienceItem).id,
+      5,
+      'Timeline should contain 1 future event in the fourth position (index 3)'
+    )
   })
 
   test('should handle when current date is the first event', ({ assert }) => {
@@ -55,16 +77,29 @@ test.group('Timeline mapper algorithm', () => {
       mockEvent(4, '2024-04-01'),
       mockEvent(5, '2024-05-01'),
     ]
-    const result = timelineDataMapper.map({ timeline, id: 1 })
+    const result = timelineDataMapper.map({ timeline, id: 1, date_des_faits: null })
     assert.lengthOf(result, 4)
-    assert.equal(result[0].id, 1, 'Current audience is the first because no past event')
+
+    assert.equal(result[0].type, 'audience', 'Current audience should be a TimelineAudienceItem')
+    assert.equal(
+      (result[0] as TimelineAudienceItem).id,
+      1,
+      'Current audience is the first because no past event'
+    )
   })
 
   test('should handle correctly empty dates', ({ assert }) => {
     const timeline = [mockEvent(1, '2024-01-01'), mockEvent(2), mockEvent(3, '2023-01-01')]
-    const result = timelineDataMapper.map({ timeline, id: 1 })
-    assert.equal(result[0].id, 3, '2023 should be first')
-    assert.equal(result[2].id, 2, 'Empry date should be the last (year 2199)')
+    const result = timelineDataMapper.map({ timeline, id: 1, date_des_faits: null })
+    assert.lengthOf(result, 3)
+    assert.equal(result[0].type, 'audience', 'First item should be a TimelineAudienceItem')
+    assert.equal((result[0] as TimelineAudienceItem).id, 3, '2023 should be first')
+    assert.equal(
+      result[2].type,
+      'audience_a_venir',
+      'Last item should be a TimelineAudienceAVenirItem'
+    )
+    assert.equal((result[2] as TimelineAudienceAVenirItem).id, 2, 'Empry date should be the last')
   })
 
   test('should return 4 events even if current is the last one', ({ assert }) => {
@@ -75,13 +110,139 @@ test.group('Timeline mapper algorithm', () => {
       mockEvent(4, '2024-04-01'),
       mockEvent(5, '2024-05-01'),
     ]
-    const result = timelineDataMapper.map({ timeline, id: 5 })
+    const result = timelineDataMapper.map({ timeline, id: 5, date_des_faits: null })
     assert.lengthOf(result, 4, 'Should fill with past events if no future events left')
   })
 
-  test('should return 1 event there is only one event', ({ assert }) => {
+  test('should return 1 event there is only one event and no date des faits', ({ assert }) => {
     const timeline = [mockEvent(1, '2024-01-01')]
-    const result = timelineDataMapper.map({ timeline, id: 1 })
+    const result = timelineDataMapper.map({ timeline, id: 1, date_des_faits: null })
     assert.lengthOf(result, 1)
+  })
+
+  test('should add date des faits at first position if provided', ({ assert }) => {
+    const timeline = [mockEvent(1, '2024-01-10')]
+    const result = timelineDataMapper.map({ timeline, id: 1, date_des_faits: '2019-01-01' })
+    assert.lengthOf(result, 2)
+    assert.equal(result[0].type, 'faits')
+    assert.equal(result[0].date, '2019-01-01')
+    assert.equal(result[1].type, 'audience')
+    assert.equal((result[1] as TimelineAudienceItem).id, 1)
+  })
+
+  test('should return 4 events max when date des faits is provided', ({ assert }) => {
+    const timeline = [
+      mockEvent(1, '2024-01-01'),
+      mockEvent(2, '2024-02-01'),
+      mockEvent(3, '2024-03-01'),
+      mockEvent(4, '2024-04-01'),
+      mockEvent(5, '2024-05-01'),
+    ]
+    const result = timelineDataMapper.map({ timeline, id: 4, date_des_faits: '2019-01-01' })
+    assert.lengthOf(result, 4, 'Should trim to maximumNumberOfEvents')
+    assert.equal(result[0].type, 'faits', 'First item should be date des faits')
+    assert.equal(result[0].date, '2019-01-01', 'Date des faits should be correct')
+    assert.equal(result[1].type, 'audience', 'Second item should be a past TimelineAudienceItem')
+    assert.equal(
+      (result[1] as TimelineAudienceItem).id,
+      3,
+      'Most recent past event should be in second position'
+    )
+    assert.equal(
+      result[2].type,
+      'audience',
+      'Third item should be the current TimelineAudienceItem'
+    )
+    assert.equal(
+      (result[2] as TimelineAudienceItem).id,
+      4,
+      'Third item should be the current event'
+    )
+    assert.equal(result[3].type, 'audience', 'Fourth item should be a future TimelineAudienceItem')
+    assert.equal(
+      (result[3] as TimelineAudienceItem).id,
+      5,
+      'Most recent future event should be in fourth position'
+    )
+  })
+
+  test('should add skipped_before and skipped_after count when audiences are skipped', ({
+    assert,
+  }) => {
+    const timeline = [
+      mockEvent(1, '2024-01-01'),
+      mockEvent(2, '2024-02-01'),
+      mockEvent(3, '2024-03-01'),
+      mockEvent(4, '2024-04-01'), //current audience
+      mockEvent(5, '2024-05-01'),
+      mockEvent(6, '2024-06-01'),
+      mockEvent(7, '2024-07-01'),
+      mockEvent(8, '2024-08-01'),
+    ]
+    const result = timelineDataMapper.map({ timeline, id: 4, date_des_faits: '2019-01-01' })
+    assert.lengthOf(result, 4, 'Should trim to maximumNumberOfEvents')
+    assert.equal(result[0].type, 'faits', 'First item should be date des faits')
+    assert.equal(result[1].type, 'audience', 'Second item should be a past TimelineAudienceItem')
+    assert.equal(
+      (result[1] as TimelineAudienceItem).skipped_before,
+      2,
+      'Second item should have skipped_before with the number of past events skipped'
+    )
+    assert.equal(
+      result[2].type,
+      'audience',
+      'Third item should be the current TimelineAudienceItem'
+    )
+    assert.equal(
+      (result[2] as TimelineAudienceItem).id,
+      4,
+      'Third item should be the current event'
+    )
+    assert.equal(
+      (result[3] as TimelineAudienceItem).id,
+      5,
+      'Most recent future event should be in fourth position'
+    )
+    assert.equal(
+      (result[3] as TimelineAudienceItem).skipped_after,
+      3,
+      'Fourth item should have skipped_after with the number of future events skipped'
+    )
+  })
+
+  test('should return older events first if current audience is not present', ({ assert }) => {
+    const timeline = [
+      mockEvent(1, '2024-01-01'),
+      mockEvent(2, '2025-05-01'),
+      mockEvent(3, '2024-03-01'),
+      mockEvent(4, '2026-02-01'),
+      mockEvent(5, '2024-04-01'),
+    ]
+    const result = timelineDataMapper.map({ timeline, id: 99, date_des_faits: '2019-01-01' })
+    assert.lengthOf(result, 4, 'Should trim to maximumNumberOfEvents')
+    assert.equal(result[0].type, 'faits', 'First item should be date des faits')
+    assert.equal(result[1].type, 'audience', 'Second item should be a TimelineAudienceItem')
+    assert.equal(
+      (result[1] as TimelineAudienceItem).id,
+      1,
+      'Second item should be the older past event'
+    )
+    assert.equal(result[2].type, 'audience', 'Third item should be a TimelineAudienceItem')
+    assert.equal(
+      (result[2] as TimelineAudienceItem).id,
+      3,
+      'Third item should be the second older past event'
+    )
+    assert.equal(result[3].type, 'audience', 'Last item should be a TimelineAudienceItem')
+    assert.equal(
+      (result[3] as TimelineAudienceItem).id,
+      5,
+      'Fourth item should be the third older past event'
+    )
+    assert.equal(
+      (result[3] as TimelineAudienceItem).skipped_after,
+      2,
+      'Fourth item should have skipped_after with the number of future events skipped'
+    )
   })
 })
