cd9e9ca0f92dbeecf7c494a0a2a68ceaa0869a6f
Jean Bui Quang <jean.buiquang@etik.com>
2026-04-20 07:18:43 +0000

    feat: update detail audience inner nav

--- start/view.ts
+++ start/view.ts
@@ -13,3 +13,28 @@ addCollection(phIcons)
 edge.use(edgeIconify)
 edge.global('env', env)
 edge.global('content', content)
+
+/**
+ * Map a juridiction label.
+ * If both juridiction and ville_de_l_audience are present, it returns "juridiction de Ville_de_l_audience"
+ * If only one of them is present, it returns the one that is present
+ * If none of them are present, it returns an empty string
+ * @param audience
+ * @returns
+ */
+const juridictionLabel = (
+  juridiction: string,
+  ville_de_l_audience: string,
+  degre_de_juridiction?: string
+) => {
+  const firstPart = [juridiction, string.capitalCase(ville_de_l_audience ?? '')]
+    .filter(Boolean)
+    .join(' de ')
+  return degre_de_juridiction ? `${firstPart} - ${degre_de_juridiction}` : firstPart
+}
+
+const dateDecisionLabel = (date_de_decision?: DateTime) => {
+  return date_de_decision?.toFormat('dd.MM.yyyy') ?? 'À venir'
+}
+
+edge.global('audienceHelper', { juridictionLabel, dateDecisionLabel })
