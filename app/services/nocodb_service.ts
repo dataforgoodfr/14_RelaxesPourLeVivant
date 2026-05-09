258602b99956e51ce275266a650019d22624075b
Yoann Bourdex <yoann.bourdex@gmail.com>
2026-05-09 11:10:12 +0200

    refactor(nocodb-api): remove useles authorization hearder for fetchAttachmentFile

--- app/services/nocodb_service.ts
+++ app/services/nocodb_service.ts
@@ -2,13 +2,8 @@ import env from '#start/env'
 
 export class NocodbService {
   private readonly baseUrl = env.get('NC_API_URL')
-  private readonly apiToken = env.get('NC_API_TOKEN')
 
   async fetchAttachmentFile(nocoDbPath: string) {
-    return fetch(`${this.baseUrl}/${nocoDbPath}`, {
-      headers: {
-        Authorization: `Bearer ${this.apiToken}`,
-      },
-    })
+    return fetch(`${this.baseUrl}/${nocoDbPath}`)
   }
 }
